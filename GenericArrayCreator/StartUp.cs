6ff52ee1422312321e927e1eb5e2f5ddf68029bf
Svetoslav Petsev <svetoslav_petsev@users.noreply.github.com>
2020-05-01 12:09:56 +0300

    Add project files.

---
+++ GenericArrayCreator/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericArrayCreator
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ GenericScale/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericScale
+{
+    using System;
+
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
